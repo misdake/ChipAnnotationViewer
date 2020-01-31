28ce72c40db7d94a3d18248a47abc90888caad45
misdake <misdake@gmail.com>
2020-01-31 22:24:44 +0800

    delete deprecated layers.

--- ts/layers/Layers.ts
+++ ts/layers/Layers.ts
@@ -1,20 +1,12 @@
 import {Canvas} from "../Canvas";
 import {LayerImage} from "./LayerImage";
 import {LayerPolylineView} from "./LayerPolylineView";
-import {LayerPolylineCreate} from "./LayerPolylineCreate";
-import {LayerPolylineEdit} from "./LayerPolylineEdit";
 import {LayerTextView} from "./LayerTextView";
-import {LayerTextCreate} from "./LayerTextCreate";
-import {LayerTextEdit} from "./LayerTextEdit";
 import {Layer} from "./Layer";
 
 export enum LayerName {
     IMAGE,
-    POLYLINE_CREATE,
-    POLYLINE_EDIT,
     POLYLINE_VIEW,
-    TEXT_CREATE,
-    TEXT_EDIT,
     TEXT_VIEW,
 }
 
@@ -24,11 +16,7 @@ export class Layers {
         return [
             new LayerImage(canvas),
             new LayerPolylineView(canvas),
-            // new LayerPolylineCreate(canvas),
-            // new LayerPolylineEdit(canvas),
             new LayerTextView(canvas),
-            // new LayerTextCreate(canvas),
-            // new LayerTextEdit(canvas),
         ];
     }
 

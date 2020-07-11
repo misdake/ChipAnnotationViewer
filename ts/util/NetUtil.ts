1257d776146283531e68acd5b77fdd8219151e9b
misdake <misdake@gmail.com>
2020-07-11 11:55:19 +0800

    add count for everyday view.

--- ts/util/NetUtil.ts
+++ ts/util/NetUtil.ts
@@ -1,9 +1,9 @@
 export class NetUtil {
-    public static get(url: string, callback: (text: string) => void) {
+    public static get(url: string, callback?: (text: string) => void) {
         let request = new XMLHttpRequest();
         request.onreadystatechange = function () {
             if (request.readyState == 4 && request.status == 200) {
-                callback(request.responseText);
+                if (callback) callback(request.responseText);
             }
         };
         request.open("GET", url, true);
@@ -11,9 +11,26 @@ export class NetUtil {
     }
 
     public static count(chip: string, comment: number) {
-        console.log("count", chip, comment)
+        let t = new Date().getTime();
+        const DAY_TIME = 1000 * 60 * 60 * 24;
+        let today = ~~(t / DAY_TIME);
+
+        let savedDayString = localStorage.getItem("today");
+        let savedDay = undefined;
+        if (savedDayString) {
+            try {
+                savedDay = parseInt(savedDayString);
+            } catch (e) {
+            }
+        }
+
+        if (!savedDay || today > savedDay) {
+            NetUtil.get(`https://api.countapi.xyz/hit/chipannotationviewer/day_${today}`);
+            localStorage.setItem("today", `${today}`);
+        }
+
+        // console.log("count", chip, comment)
         let url = `https://api.countapi.xyz/hit/chipannotationviewer/${chip}_${comment}`;
-        NetUtil.get(url, text => {
-        });
+        NetUtil.get(url);
     }
 }
